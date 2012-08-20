30a084f61537679d60109932eaecdd2dc112edcc
Jishnu <jishnu7@gmail.com>
2012-08-20 10:39:37 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = 'payyans'
+
+setup(
+    name = name,
+    version = "0.1",
+    url = "http://silpa.org.in/payyans",
+    license = "LGPL 3.0",
+    description = "ASCII to Unicode converter",
+    author = 'Santhosh Thottingal',
+    author_email = "santhosh.thottingal@gmail.com",
+    long_description = """This library helps to convert 
+ascii texts to unicode.""",
+    packages=find_packages('.'),
+    package_data = {'.':['payyans/maps']},
+    include_package_data = True,
+    setup_requires = ['setuptools'],
+    install_requires = ['setuptools', 'normalizer'],
+    zip_safe = False,
+    )
