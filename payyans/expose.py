b839606d47cca5c14fa3d97744ba4effdf5064e4
Jerin Philip <monu1618@gmail.com>
2016-08-12 10:34:53 +0530

    WSME REST Capability
    Necessary functions defined in payyans/expose.py
    setup.py, setup.cfg modified to use pbr and added entrypoints to expose.

---
+++ payyans/expose.py
@@ -0,0 +1,20 @@
+from payyans import Payyans
+
+
+def payyans_unicode2ascii(text, font):
+    P = Payyans()
+    return P.Unicode2ASCII(text, font)
+
+
+def payyans_ascii2unicode(text, font):
+    P = Payyans()
+    return P.ASCII2Unicode(text, font)
+
+def ascii2unicode():
+    return [payyans_ascii2unicode, str, str, str]
+
+def unicode2ascii():
+    return [payyans_unicode2ascii, str, str, str]
+
+
+
--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
-
-from setuptools import setup,find_packages
-
-name = 'payyans'
+from setuptools import setup
 
 setup(
-    name = name,
-    version = "0.2",
-    url = "http://silpa.org.in/payyans",
-    license = "LGPL 3.0",
-    description = "ASCII to Unicode converter",
-    author = 'Santhosh Thottingal',
-    author_email = "santhosh.thottingal@gmail.com",
-    long_description = """This library helps to convert
-ascii texts to unicode.""",
-    packages=find_packages(),
-    include_package_data = True,
-    setup_requires = ['setuptools-git'],
-    install_requires = ['setuptools', 'normalizer'],
-    zip_safe = False,
-    )
+    setup_requires=['pbr'],
+    pbr=True
+)
+
